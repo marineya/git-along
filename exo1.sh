30c2a3a3a9a095ea083843e505d4dccc5c03dd41
marine.yang@sorbonne-nouvelle.fr <marine.yang@sorbonne-nouvelle.fr>
2025-10-15 09:39:02 +0200

    test

---
+++ exo1.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+echo "Séance 3. 13/10/2025"
+echo "Exercice 1"
+
+echo "Nombre de ligne contenant Location des fichiers ann"
+
+echo "Nombre de Location en 2016"
+cat 2016/* | grep Location | wc -l >> exo1.sh
+3144
+
+echo "Nombre de Location en 2017"
+cat 2017/* | grep Location | wc -l >> exo1.sh
+2466
+
+echo "Nombre de Location en 2018"
+cat 2018/* | grep Location | wc -l >> exo1.sh
+3110
