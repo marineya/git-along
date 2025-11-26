c4557719e0adaea08892683886abc39c272f1cc8
marine.yang@sorbonne-nouvelle.fr <marine.yang@sorbonne-nouvelle.fr>
2025-11-26 01:57:46 +0100

    ...

---
+++ Exercices/compte.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+echo"argument donné : $1"
+
+CHEMIN=$1
+
+echo "Nombre de lieux en 2016:"
+cat "$CHEMIN/2016/"* | grep Location | wc -l
+
+echo "Nombre de lieux en 2017:"
+cat "$CHEMIN/2017/"* | grep Location | wc -l
+
+
+echo "Nombre de lieux en 2018:"
+cat "$CHEMIN/2018/"* | grep Location | wc -l
